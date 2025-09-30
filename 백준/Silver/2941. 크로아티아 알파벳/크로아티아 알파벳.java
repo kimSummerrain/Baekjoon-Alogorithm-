9b0dcf0bc0452be93eb324019e77ad92f1f72c3a
kimSummerrain <99111878+kimSummerrain@users.noreply.github.com>
2025-10-01 01:14:44 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String word=br.readLine();
+
+        List<String> arrs= Arrays.asList("c=","c-","dz=",
+                "d-","lj","nj","s=","z=");
+        String replaced = arrs.stream()
+                .reduce(word,(w,pat)->w.replace(pat,"*"))
+;
+        System.out.println(replaced.length());
+
+
+        
+
+    }
+}
