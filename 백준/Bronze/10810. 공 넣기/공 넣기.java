896a0ac01cb834015541af914b2890fe409cab85
kimSummerrain <99111878+kimSummerrain@users.noreply.github.com>
2025-10-11 14:25:44 +0900

    [Bronze III] Title: 공 넣기, Time: 200 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(0);
+        }
+        for(int i=0;i<m;i++){
+            int a=sc.nextInt()-1;
+            int b=sc.nextInt()-1;
+            int c=sc.nextInt();
+            if(a!=b){
+            for(int j=a;j<=b;j++){
+                list.set(j,c);
+                }
+            }else{
+                list.set(a,c);
+            }
+        }
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+
+        }
+    }
+}
