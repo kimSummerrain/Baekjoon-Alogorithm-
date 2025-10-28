4e6a59160dc43f4b5061fa0d87eb182571139a56
kimSummerrain <99111878+kimSummerrain@users.noreply.github.com>
2025-10-28 09:12:50 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = 0;
+        for (int i = 0; i < 3; i++) {
+            String str = br.readLine();
+            if (Character.isDigit(str.charAt(0))) num = Integer.parseInt(str) + (3 - i);
+        }
+        if (num % 15 == 0) System.out.println("FizzBuzz");
+        else if (num % 3 == 0) System.out.println("Fizz");
+        else if (num % 5 == 0) System.out.println("Buzz");
+        else System.out.println(num);
+    }
+}
