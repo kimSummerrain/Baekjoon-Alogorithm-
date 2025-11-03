fa8a2301fb74407182e859963be501e6101c6e9b
kimSummerrain <99111878+kimSummerrain@users.noreply.github.com>
2025-11-03 11:24:00 +0900

    [Bronze III] Title: 부호, Time: 1484 ms, Memory: 311176 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.java
@@ -0,0 +1,36 @@
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> result = new ArrayList<>();
+        while(true){
+            try{
+            int num = sc.nextInt();
+            BigInteger sum = BigInteger.ZERO;
+
+            for(int i=0;i<num;i++){
+                sum=sum.add(sc.nextBigInteger());
+            }
+
+            if(sum.compareTo(BigInteger.ZERO)>0){
+                result.add('+');
+            }else if(sum.compareTo(BigInteger.ZERO)<0){
+                result.add('-');
+            }else{
+                result.add('0');
+            }
+
+        }catch(Exception e){
+            break;}
+        }
+        for (char c : result) {
+            System.out.println(c);
+        
+    }
+    
+    }
+}
