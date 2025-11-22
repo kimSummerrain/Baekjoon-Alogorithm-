e3962d08829465211478e93531c1e4353d334c12
kimSummerrain <99111878+kimSummerrain@users.noreply.github.com>
2025-11-22 21:03:14 +0900

    [Bronze II] Title: 하얀 칸, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < 8; j++) {
+                if (s.charAt(j) == 'F' && (i + j) % 2 == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
